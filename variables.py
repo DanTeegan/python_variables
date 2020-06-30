606d16b1b5d9892060410e1b42c30a5ea97d258e
dan. teegan <dan.teegan@hotmail.com>
2020-06-30 17:22:41 +0100

    Python variables

---
+++ variables.py
@@ -0,0 +1,50 @@
+
+# print(" Hello world ")
+
+# Let's create a variable
+
+# x = 10  # Type of integer
+# y = 3.6 # Type of float (Decimal)
+# name = "Sparta" # Type of string
+#
+# print(x)
+# print(y)
+# print(name)
+# print(x + y)
+#
+# # Built in method called type()
+#
+# print(type(name)) # Returns the type of data in this case it is a string
+#
+# print( x + name ) # You cannot add together data types of string and int
+
+#print(str(x) + name) # You must convert the INT into a string using the str() function. This is known as casting.
+
+# age = 99
+# NHS = 1231312
+# name = input("Please enter your name")
+# salary = 50000
+
+# name = input(" Please enter your name ")
+# age = input(" Please enter your age ")
+# print(name)
+# print(age)
+
+# Overwriting a variable
+# name = "James"
+# print(name)
+# name = "bond"
+# print(name)
+
+# Exercise
+
+# create a variable called first_name and last_name
+# create a variable called full_name and display full_name
+
+first_name = input("Please enter your first name")
+last_name = input("Please enter your last name")
+full_name = (first_name + last_name)
+age = input("Please enter your age")
+adress = input("Please enter your adress")
+
+print(full_name)
